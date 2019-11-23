1e269a6c87d0d6c7ea51b41937f541ce33ce2de5
rlamug <rlamug@mail.csuchico.edu>
2019-11-22 18:59:13 -0800

    Added placePiece Tests

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -18,21 +18,51 @@ TEST(TicTacToeBoardTest, unitTestName) {
   ASSERT_TRUE(true);
 }
 */
+
 // Sanity Check
 TEST(TicTacToeBoardTest, sanityCheck) {
   TicTacToeBoard ticTacToeBoard;
   ASSERT_TRUE(true);
 }
+
 /*** toggleTurn ***/
-// Test Initial toggleTurn()
+// toggleTurn() when turn = X
 TEST(TicTacToeBoardTest, toggleTurnX) {
   TicTacToeBoard ticTacToeBoard;
   ASSERT_EQ(ticTacToeBoard.toggleTurn(), 'O');
 }
-// Test Second toggleTurn()
+// toggleTurn() when turn = O
 TEST(TicTacToeBoardTest, toggleTurnO) {
   TicTacToeBoard ticTacToeBoard;
   ticTacToeBoard.toggleTurn();
   ASSERT_EQ(ticTacToeBoard.toggleTurn(), 'X');
 }
 
+/*** placePiece ***/
+// Places piece on blank coordinate
+TEST(TicTacToeBoardTest, placePieceBlank) {
+  TicTacToeBoard ticTacToeBoard;
+  ASSERT_EQ(ticTacToeBoard.placePiece(0, 0), 'X');
+}
+// Places piece on coordinate already occupied
+TEST(TicTacToeBoardTest, placePieceOccupied) {
+  TicTacToeBoard ticTacToeBoard;
+  ticTacToeBoard.placePiece(0, 0);
+  ASSERT_EQ(ticTacToeBoard.placePiece(0, 0), 'X');
+}
+// Places piece on invalid coordinate
+TEST(TicTacToeBoardTest, placePieceInvalid) {
+  TicTacToeBoard ticTacToeBoard;
+  ASSERT_EQ(ticTacToeBoard.placePiece(3, 3), '?');
+}
+// Shouldn't place piece when game is over
+TEST(TicTacToeBoardTest, placePieceGameOver) {
+  TicTacToeBoard ticTacToeBoard;
+  ticTacToeBoard.placePiece(0, 0);
+  ticTacToeBoard.placePiece(1, 0);
+  ticTacToeBoard.placePiece(0, 1);
+  ticTacToeBoard.placePiece(1, 1);
+  ticTacToeBoard.placePiece(0, 2);
+  ticTacToeBoard.placePiece(1, 3);
+  ASSERT_EQ(ticTacToeBoard.getPiece(1, 3), ' ');
+}
