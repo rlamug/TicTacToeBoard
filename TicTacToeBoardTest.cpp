d1d00df6b4ff18f562161086db6eb870da9b4a7f
rlamug <rlamug@mail.csuchico.edu>
2019-11-22 17:56:16 -0800

    Added toggleTurnX Test

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -1,25 +1,30 @@
 /**
  * Unit Tests for TicTacToeBoard
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "TicTacToeBoard.h"
- 
+
 class TicTacToeBoardTest : public ::testing::Test {
-	protected:
-		TicTacToeBoardTest(){} //constructor runs before each test
-		virtual ~TicTacToeBoardTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+ protected:
+  TicTacToeBoardTest() {}           // constructor runs before each test
+  virtual ~TicTacToeBoardTest() {}  // destructor cleans up after tests
+  virtual void SetUp() {}     // sets up before each test (after constructor)
+  virtual void TearDown() {}  // clean up after each test, (before destructor)
 };
 
 /* EXAMPLE TEST FORMAT
 TEST(TicTacToeBoardTest, unitTestName) {
-	ASSERT_TRUE(true);
+  ASSERT_TRUE(true);
 }
 */
 
 TEST(TicTacToeBoardTest, sanityCheck) {
-	TicTacToeBoard ticTacToeBoard;
-	ASSERT_TRUE(true);
+  TicTacToeBoard ticTacToeBoard;
+  ASSERT_TRUE(true);
+}
+
+TEST(TicTacToeBoardTest, toggleTurnX) {
+  TicTacToeBoard ticTacToeBoard;
+  ASSERT_EQ(ticTacToeBoard.toggleTurn(), X);
 }
